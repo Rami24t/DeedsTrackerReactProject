5f4aa4acb0fea8e2e18a575fa49ad11a26f61a34
RamS24 <AlSaadi.Rami24@gmail.com>
2022-10-23 08:24:47 +0200

    style delete-icon

--- src/components/Deed.js
+++ src/components/Deed.js
@@ -8,17 +8,16 @@ const Deed = ({ deed, onDelete, onToggle }) => {
       className={'deed clearfix ' + (deed.important ? ' important ' : '')}
       onDoubleClick={() => {
         onToggle(deed.id);
-      }}
-    >
+      }}>
       <h3 className="clearfix">
         {deed.text}
-        <FaTimes
+      </h3>
+      <FaTimes
           className="delete-icon"
           title='delete'
           style={{ color: 'red', cursor: 'pointer' }}
           onClick={() => onDelete(deed.id)}
         />
-      </h3>
       <p> {deed.date} </p>
       <Link to={('/deed/' + deed.id)}>Details:...</Link>
     </div>
